59cde2ec875f13e5d73ba7a870edfa790e6886c5
andyliu-417 <andy2018.liu@gmail.com>
2018-09-18 16:32:19 -0400

    comment and ignore

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux-immutable";
-import { reducer as headerReducer } from "../components/Header/store";
+// import { reducer as headerReducer } from "../components/Header/store";
 
 export default combineReducers({
-  header: headerReducer
+  // header: headerReducer
 });
